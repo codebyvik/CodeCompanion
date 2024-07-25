2d3d76b8fa48497ca99302545019cb00fbd67190
codebyvik <vikasvickycoorg@gmail.com>
2024-07-25 16:55:27 +0530

    added navbar and theme settings

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import Navbar from "@/components/shared/navbar/Navbar";
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="background-light-850_dark-100 relative">
+      <Navbar />
+      <div className="flex">
+        leftbar
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+        rightbar
+      </div>
+      Toaster
+    </main>
+  );
+};
+
+export default Layout;
