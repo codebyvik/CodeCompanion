a765095fa02fc83170a3597479552bff40af1017
codebyvik <vikasvickycoorg@gmail.com>
2024-07-29 14:00:46 +0530

    added webhooks and user actions

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,9 @@
 
 import UserModel from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import { CreateUserParams, DeleteUserParams, UpdateUserParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
+import QuestionModel from "@/database/question.model";
 
 export async function getUserByID(params: any) {
   try {
@@ -11,6 +14,57 @@ export async function getUserByID(params: any) {
     const user = await UserModel.findOne({ clerkId: userId });
 
     return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function CreateUser(userData: CreateUserParams) {
+  try {
+    await connectToDatabase();
+    const newUser = await UserModel.create(userData);
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+export async function UpdateUser(userData: UpdateUserParams) {
+  try {
+    await connectToDatabase();
+    const { clerkId, updateData, path } = userData;
+    await UserModel.findOneAndUpdate({ clerkId }, updateData, { new: true });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+export async function DeleteUser(userData: DeleteUserParams) {
+  try {
+    await connectToDatabase();
+    const { clerkId } = userData;
+    const user = await UserModel.findOne({ clerkId });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // delete user from mongoDB , related questions , answers and comments
+
+    // get question Id's
+    // const userQuestionIds = await QuestionModel.find({ author: user._id }).distinct("_id");
+    // delete questions
+    await QuestionModel.deleteMany({ author: user._id });
+
+    // todo: delete user answers,comments,etc
+
+    const deletedUser = await UserModel.findByIdAndDelete(user._id);
+
+    return deletedUser;
   } catch (error) {
     console.log(error);
     throw error;
