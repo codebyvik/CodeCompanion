3af561d74a7e062e69631f712ff770314d74b762
codebyvik <vikasvickycoorg@gmail.com>
2024-08-02 22:26:19 +0530

    bug fixes and added recommended questions

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -99,7 +99,7 @@ const Page = async ({ params, searchParams }: URLProps) => {
               Answers
             </TabsTrigger>
           </TabsList>
-          <TabsContent value="top-posts">
+          <TabsContent className="mt-5 flex w-full flex-col gap-6" value="top-posts">
             <QuestionTab searchParams={searchParams} userId={userInfo.user._id} clerkId={clerkId} />
           </TabsContent>
           <TabsContent value="answers" className="flex w-full flex-col gap-6">
